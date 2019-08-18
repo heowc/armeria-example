4bf2f66373d47a68a8e1109e89f9d6d10d31d2e6
heowc <heowc1992@gmail.com>
2019-08-19 00:30:21 +0900

    Initial commit

---
+++ getting-started/src/main/java/com/example/GettingStartedApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+
+import java.util.concurrent.CompletableFuture;
+
+public class GettingStartedApplication {
+
+    public static void main(String[] args) {
+        ServerBuilder sb = new ServerBuilder();
+        Server server = sb.http(8080)
+                .service("/", (ctx, res) -> HttpResponse.of("Hello, Armeria!"))
+                .build();
+        CompletableFuture<Void> future = server.start();
+        future.join();
+    }
+}
