86540d3bc41d79f33ad94a26e06ef73b37fc91bc
heowc <heowc1992@gmail.com>
2019-08-27 01:12:47 +0900

    Add springboot example

---
+++ springboot-integration/src/main/java/com/example/ArmeriaConfig.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.linecorp.armeria.server.docs.DocService;
+import com.linecorp.armeria.server.logging.AccessLogWriter;
+import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ArmeriaConfig {
+
+    @Bean
+    public ArmeriaServerConfigurator armeriaServerConfigurator(IndexService indexService) {
+        return builder -> {
+            builder.serviceUnder("/docs", new DocService());
+            builder.decorator(LoggingService.newDecorator());
+            builder.accessLogWriter(AccessLogWriter.combined(), false);
+            builder.annotatedService(indexService);
+        };
+    }
+}
